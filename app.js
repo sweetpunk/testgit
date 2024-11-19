40aa698d7e27de572d061db320306f0c0ab984de
louis <sweetpunk42442@gmail.com>
2024-11-19 14:17:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+
+
+const { Schema } = mongoose;
+
+const blogSchema = new Schema({
+  title: String, // String is shorthand for {type: String}
+  author: String,
+  body: String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number
+  }
+});
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
